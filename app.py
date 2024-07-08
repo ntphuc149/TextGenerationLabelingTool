219efd65593fa4470e4e86f72482012bb533764d
Truong-Phuc Nguyen <99306941+ntphuc149@users.noreply.github.com>
2024-07-09 01:14:59 +0700

    Update app.py
    Update evaluation metrics

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import os
 import requests
 
-st.set_page_config(page_icon='🍃', page_title='Text Generation Labeling Tool', layout='wide', initial_sidebar_state="collapsed")
-st.markdown("<h1 style='text-align: center;'>Text Generation Labeling Tool</h1>", unsafe_allow_html=True)
+st.set_page_config(page_icon='🍃', page_title='Text Generation Labeling Tool by n.t.phuc149 🌒', layout='wide', initial_sidebar_state="collapsed")
+st.markdown("<h1 style='text-align: center;'>Text Generation Labeling Tool by n.t.phuc149 🌒</h1>", unsafe_allow_html=True)
 
-def file_selector(folder_path=r'./DemoDatasets'):
+def file_selector(folder_path=r'./Datasets'):
     filenames = os.listdir(folder_path)
     return filenames, folder_path
 
@@ -57,7 +57,37 @@ if len(df) != 0:
     col_21, col_22 = st.columns(spec=[9, 1])
     txt_distractors = col_21.text_area(height=90, label='Your distractors:', label_visibility='collapsed', value=df['distract'][st.session_state.idx])  
     btn_generate_distractor = col_22.button(label='Generate distractors', use_container_width=True)
+
+    col_eval_1, col_eval_2, col_eval_3, col_eval_4, col_eval_5, col_eval_6, col_eval_7, col_eval_8, col_eval_9, col_eval_10 = st.columns(spec=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+
+    options = ['UNRATED', 'BAD', 'ACCEPTABLE', 'GOOD']
+    q_fluency_value = df['q_fluency'][st.session_state.idx]
+    q_clarity_value = df['q_clarity'][st.session_state.idx]
+    q_conciseness_value = df['q_conciseness'][st.session_state.idx]
+
+    qa_relevance_value = df['qa_relevance'][st.session_state.idx]
+    qa_consistency_value = df['qa_consistency'][st.session_state.idx]
+    qa_answer_correctness_value = df['qa_answer_correctness'][st.session_state.idx]
+    qa_answer_consistency_value = df['qa_answer_consistency'][st.session_state.idx]
+
+    d_correctness_value = df['d_correctness'][st.session_state.idx]
+    d_understanding_value = df['d_understanding'][st.session_state.idx]
+    d_difficulty_value = df['d_difficulty'][st.session_state.idx]
     
+    q_fluency = col_eval_1.selectbox(label='[Q] Fluency:', options=options, index=options.index(q_fluency_value))
+    q_clarity = col_eval_2.selectbox(label='[Q] Clarity:', options=options, index=options.index(q_clarity_value))
+    q_conciseness = col_eval_3.selectbox(label='[Q] Conciseness:', options=options, index=options.index(q_conciseness_value))
+
+    qa_relevance = col_eval_4.selectbox(label='[QA] Relevance:', options=options, index=options.index(qa_relevance_value))
+    qa_consistency = col_eval_5.selectbox(label='[QA] Consistency:', options=options, index=options.index(qa_consistency_value))
+    qa_answer_correctness = col_eval_6.selectbox(label='[QA] Answer correctness:', options=options, index=options.index(qa_answer_correctness_value))
+    qa_answer_consistency = col_eval_7.selectbox(label='[QA] Answer consistency:', options=options, index=options.index(qa_answer_consistency_value))
+
+    
+    d_correctness = col_eval_8.selectbox(label='[D] Correctness:', options=options, index=options.index(d_correctness_value))
+    d_understanding = col_eval_9.selectbox(label='[D] Understanding:', options=options, index=options.index(d_understanding_value))
+    d_difficulty = col_eval_10.selectbox(label='[D] Difficulty:', options=options, index=options.index(d_difficulty_value))
+
     if btn_generate_distractor:
         if filename_input == 'BiologyQA.csv':
             expert = 'biologist'
@@ -105,12 +135,26 @@ if len(df) != 0:
 
     if btn_save:
         df['context'][st.session_state.idx] = txt_context
-        df['question'][st.session_state.idx] = txt_question
+        df['[Q]'][st.session_state.idx] = txt_question
         df['answer'][st.session_state.idx] = txt_answer
         df['distract'][st.session_state.idx] = txt_distractors
-        df['question_type'][st.session_state.idx] = revert_question_type_id(txt_question_type)
+        df['[Q]_type'][st.session_state.idx] = revert_question_type_id(txt_question_type)
 
-        df.to_csv(f'./DemoDatasets/{filename_input}', index=None)
+        df['q_fluency'][st.session_state.idx] = q_fluency
+        df['q_clarity'][st.session_state.idx] = q_clarity
+        df['q_conciseness'][st.session_state.idx] = q_conciseness
+
+        df['qa_relevance'][st.session_state.idx] = qa_relevance
+        df['qa_consistency'][st.session_state.idx] = qa_consistency
+        df['qa_answer_correctness'][st.session_state.idx] = qa_answer_correctness
+        df['qa_answer_consistency'][st.session_state.idx] = qa_answer_consistency
+
+        df['d_correctness'][st.session_state.idx] = d_correctness
+        df['d_understanding'][st.session_state.idx] = d_understanding
+        df['d_difficulty'][st.session_state.idx] = d_difficulty
+
+        df.to_csv(f'./Datasets/{filename_input}', index=None, encoding='utf-8-sig')
+        st.rerun()
 
     if btn_goto:
         st.session_state.idx = txt_goto - 1
